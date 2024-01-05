37cc1078713f1da5414b53dd501c496b907d36a1
MarkoSopta <philoyd99@gmail.com>
2024-01-05 14:28:32 +0100

    Zadatak6 i Zadatak8

---
+++ Zadatak8/Zadatak8.cc
@@ -0,0 +1,33 @@
+#include <mpi.h>
+#include <iostream>
+#include <vector>
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+   
+    std::vector<int> localVector(100, rank + 1);  // Primer: punjenje lokalnog vektora vrednostima rank + 1
+
+   
+    std::vector<int> globalVector(size * 100);
+    MPI_Gather(localVector.data(), 100, MPI_INT, globalVector.data(), 100, MPI_INT, 0, MPI_COMM_WORLD);
+
+   
+    if (rank == 0) {
+        int sum = 0;
+        for (int i = 0; i < size * 100; ++i) {
+            sum += globalVector[i];
+        }
+        double average = static_cast<double>(sum) / (size * 100);
+
+       
+        std::cout << "Srednja vrednost globalnog vektora: " << average << std::endl;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
