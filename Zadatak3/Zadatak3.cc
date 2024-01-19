2105f22be8a2d78e5e2cace9a169fea33f2a4b87
MarkoSopta <philoyd99@gmail.com>
2024-01-19 17:39:11 +0100

    Zadatak3

--- Zadatak3/Zadatak3.cc
+++ Zadatak3/Zadatak3.cc
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 
 Za slanje ranga procesa 0 upotrijebljen je MPI::Comm.Bcast svim
@@ -11,91 +10,46 @@ korištenjem C++ funkcija.
 */
 
 
-#include <mpi.h>
 #include <iostream>
-#include <vector>
+#include <string>
+#include <mpi.h>
+using namespace std;
 
-int main(int argc, char** argv) {
-    MPI::Init(argc, argv);
+int main(int argc, char* argv[]) {
+    int rang;
+    int size;
+    MPI::Request zahtjev;
+    MPI::Status status;    
+    char a[100], b[100];   // Polja za pohranu stringova
+    MPI::Init(argc, argv); 
+    rang = MPI::COMM_WORLD.Get_rank(); 
+    size = MPI::COMM_WORLD.Get_size(); 
 
-    int rank = MPI::COMM_WORLD.Get_rank();
-    int size = MPI::COMM_WORLD.Get_size();
+    
+    if (rang == 0) {
+        
+        MPI::COMM_WORLD.Bcast(&a, 100, MPI::CHAR, 0);
 
-    const int computerNameMaxLength = MPI_MAX_PROCESSOR_NAME;
-    char computerName[computerNameMaxLength];
-    int nameLength;
-
-    // Proces 0 šalje svoj rang svim procesima
-    MPI::COMM_WORLD.Bcast(&rank, 1, MPI::INT, 0);
-
-    if (rank != 0) {
-        // Ostali procesi šalju svoje računalno ime procesu 0
-        MPI::Get_processor_name(computerName, nameLength);
-        MPI::COMM_WORLD.Send(computerName, nameLength, MPI::CHAR, 0, 0);
-    } else {
-        // Proces 0 prima poruke od ostalih procesa
-        std::vector<MPI::Request> requests;
-
-        for (int source = 1; source < size; ++source) {
-            MPI::COMM_WORLD.Irecv(computerName, computerNameMaxLength, MPI::CHAR, source, 0);
-
-            // Dodaj request u vektor
-            requests.push_back(MPI::COMM_WORLD.Irecv(computerName, computerNameMaxLength, MPI::CHAR, source, 0));
+        int i = 0;
+        
+        while (i < size - 1) {
+            // Asinkrono primanje poruke
+            zahtjev = MPI::COMM_WORLD.Irecv(&b, 100, MPI::CHAR, MPI::ANY_SOURCE, 25);
+            zahtjev.Wait(status); // Čekanje na završetak primanja
+            i++;
+            cout << "Proces 0 primio je poruku od instance " << b << endl;
         }
-
-        // Čekaj završetak svih asinkronih operacija
-        MPI::Request::Waitall(requests.size(), requests.data());
-
-        // Ispis primljenih poruka
-        for (int source = 1; source < size; ++source) {
-            std::cout << "Primljena poruka od procesa " << source << ": " << computerName << std::endl;
-        }
+    }
+    
+    else if (rang > 0) {
+        
+        sprintf(a, "%d", rang);
+        // Slanje stringa 'a' glavnom procesu (rang 0)
+        MPI::COMM_WORLD.Isend(&a, 100, MPI::CHAR, 0, 25);
     }
 
     MPI::Finalize();
     return 0;
 }
 
-
-=======
-
-
-#include <iostream>
-#include <mpi.h>
-using namespace std;
-int main (int argc, char* argv[]) {
-        int rang;
-        int size;
-        MPI::Request zahtjev;
-        MPI::Status status;
-
-        MPI::Init(argc, argv); 
-        rang = MPI::COMM_WORLD.Get_rank(); 
-        size = MPI::COMM_WORLD.Get_size(); 
-        int a[size][size], b[size];
-        if( rang == 0 ) {
-                for (int i=0;i<size;i++){
-                        for (int j=0;j<size;j++){
-                                a[i][j]=i*j;
-                        }
-
-                }
-        }
-        MPI::COMM_WORLD.Scatter(a, size, MPI::INT, b,size,MPI::INT,0); // slanje svima isti broj poruka
-        if(rang!=0){
-                MPI::COMM_WORLD.Recv(a,1,MPI::INT,rang-1,10);
-        }
-        cout<<"Proces "<<rang<<": ";
-        for (int i=0;i<=rang;i++){
-                cout<<b[i]<<" ";
-        }
-        cout<<endl;
-
-        if(rang<size-1){
-        MPI::COMM_WORLD.Send(a,1,MPI::INT,rang+1,10); // 1 sender 1 recv
-        }
-        MPI::Finalize(); /* Zavrsi MPI */
-        return 0;
-}
-
->>>>>>> 9bb5630629e9b0103bf01869685076081de4328a
+ 
--- Zadatak6/Zadatak6.cc
+++ Zadatak6/Zadatak6.cc
@@ -6,56 +6,6 @@
 
 
 int generateRandomNumber() {
-<<<<<<< HEAD
-return rand() % 100; // Možete prilagoditi raspon prema vašim potrebama
-}
-
-// Funkcija za izračun aritmetičke sume
-int calculateArithmeticSum(const std::vector<int>& numbers) {
-int sum = 0;
-for (int num : numbers) {
-sum += num;
-}
-return sum;
-}
-
-int main(int argc, char** argv) {
-MPI_Init(&argc, &argv);
-
-int world_size, world_rank;
-world_size = MPI::COMM_WORLD.Get_size();
-world_rank = MPI::COMM_WORLD.Get_rank();
-
-srand(time(NULL) + world_rank); // Inicijalizacija generatora nasumičnih brojeva
-
-int random_number = generateRandomNumber();
-
-std::vector<int> all_numbers(world_size);
-
-MPI_Gather(&random_number, 1, MPI_INT, &all_numbers[0], 1, MPI_INT, 0, MPI::COMM_WORLD);
-
-if (world_rank == 0) {
-int arithmetic_sum = calculateArithmeticSum(all_numbers);
-double arithmetic_mean = static_cast<double>(arithmetic_sum) / world_size;
-
-MPI_Bcast(&arithmetic_mean, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-for (int i = 0; i < world_size; ++i) {
-double deviation_percent = ((double)all_numbers[i] - arithmetic_mean) / arithmetic_mean * 100.0;
-std::cout << "Process " << i << ": Deviation = " << deviation_percent << "%" << std::endl;
-}
-}else {
-double arithmetic_mean;
-MPI_Bcast(&arithmetic_mean, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-double deviation_percent = ((double)random_number - arithmetic_mean) / arithmetic_mean * 100.0;
-std::cout << "Process " << world_rank << ": Deviation = " << deviation_percent << "%" << std::endl;
-}
-
-MPI_Finalize();
-
-return 0;
-=======
     return rand() % 100;
 }
 
@@ -105,5 +55,4 @@ int main(int argc, char** argv) {
     MPI_Finalize();
 
     return 0;
->>>>>>> 9bb5630629e9b0103bf01869685076081de4328a
 }
