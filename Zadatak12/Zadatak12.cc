ab6403984c3e3d58627da237452c08c8765fa4e0
MarkoSopta <philoyd99@gmail.com>
2024-05-07 13:38:45 +0200

    Zadatak12

--- Zadatak12/Zadatak12.cc
+++ Zadatak12/Zadatak12.cc
@@ -1,52 +1,50 @@
-/*
-Napravite program koji izračunava zbroj prilagođenih prefiksa dan
-u nastavku za 4 različita niza i nakon toga ih sve zbrojiti.
-Upotrijebite OpenMP directive za paralelno izvršavanje.
- A(1) = A(1)
- A(2) = A(2) + A(1)
- A(i) = A(i-2) + A(i-1)
-*/
-
+#include <iostream>
+#include <vector>
 #include <omp.h>
-#include <stdio.h>
-#include <stdlib.h>
-#define N 10
-int main(void) {
-  int x[N], y[N], z[N], c[N], zb[N];
-  int i, sum =0; 
+using namespace std;
 
-  for (i = 0; i < N; i++) {
-    x[i] = i+1;
-    y[i] = i*2;
-    z[i] = i+2;
-    c[i] = i*3;
-  }
-  
-  omp_set_num_threads(4);
-  #pragma omp parallel shared(x, y, z, c, zb) private(i)
-  {
-    #pragma omp for
-    for (int i = 0; i < N; i++) {
-        
-	if(i==1) {
-	   x[i]+=x[0];
-	   y[i]+=y[0];
-	   z[i]+=z[0];
-	   c[i]+=c[0];
-	}
-		
-	if(i>1) {
-	   x[i]=x[i-2]+x[i-1];  // Prethodna vrijednost + sljedeća
-           y[i]=y[i-2]+y[i-1];
-           z[i]=z[i-2]+z[i-1];
-           c[i]=c[i-2]+c[i-1];
-	}
-		
-        zb[i]=x[i]+y[i]+z[i]+c[i];
-        sum+=zb[i];
+int main() {
+    const int N = 4;
+    const int M = 100;
+    vector<vector<int>> A(N, vector<int>(M, 1));
+    int trh_numb;
+    double start_time, end_time;
 
+    
+    start_time = omp_get_wtime();
+
+    omp_set_num_threads(10);
+
+    #pragma omp parallel
+    {
+        #pragma omp for
+        for(int n = 0; n < N; ++n) {
+            for(int i = 2; i < M; ++i) {
+                if(i == 2) {
+                    A[n][i] += A[n][i-1];
+                } else if(i == 1) {
+                    A[n][i] = A[n][i];
+                } else {
+                    A[n][i] = A[n][i-2] + A[n][i-1];
+                }
+            }
+        }
     }
-  }
-    printf("Ukupna suma 4 niza je: %d  \n\n",sum);
 
+    
+    end_time = omp_get_wtime();
+
+    
+    int total_sum = 0;
+    for(int n = 0; n < N; ++n) {
+        for(int i = 0; i < M; ++i) {
+            total_sum += A[n][i];
+        }
+    }
+
+    
+    cout << "Ukupni zbroj je: " << total_sum << endl;
+    cout << "Vrijeme izvršenja: " << end_time - start_time << " sekundi" << endl;
+
+    return 0;
 }
